8aa1038a9c089ac440a8c56ccb113f33eaa5df81
sauravskv07 <sauravvirmaniskv@gmail.com>
2019-08-20 23:32:11 +0530

    Completed most of the frontend

--- server.js
+++ server.js
@@ -1,17 +1,132 @@
-const express=require('express');
-const port=3000;
-const app=express();
+var express=require('express');
+var bodyParser=require('body-parser');
+var mongoose = require('mongoose');
+var config = require('./config.json');
+var preData= require('./models/User.js');
+var regVoter= require('./models/RegVoter.js');
+var speakeasy= require('speakeasy')
+var twilioClient = require('twilio')(config.accountSid, config.authToken);
+
+app=express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+mongoose.connect("mongodb://localhost:8080/voter_data");
 
 app.use(express.static('./src'));
 app.use(express.static('./build/contracts'));
 app.use(express.static('views'));
 app.use(express.static('./node_modules'));
 
-app.get('/',(req,res)=>{
-    res.sendFile('index.html');
-})
 
 
-app.listen(port,()=>{
-    console.log('app is listening on port number ',port);
+function ageCalc(x) {
+  return Math.floor(x/1000/60/60/24/365);
+}
+
+
+app.get('/', function(req,res){
+  res.render('index');
+});
+
+
+app.get('/register', function(req,res){
+  res.render('register');
+});
+
+app.get('/register/:adhar_id', function(req,res){
+  preData.find(req.params.adhar_id, function(err, found){
+    if(err) {
+      res.send('Data not found');
+      res.redirect('/register'); }
+    else {
+      var presentDate= Date.now();
+      var birthDate= new Date(found.dob);
+      var age= ageCalc(presentDate-birthDate);
+      if (age>=18) {
+        regVoter.create(found, function(err,success){
+          if(err) {
+            res.send('Something went wrong. Please try again.');
+          } else {
+            var secret= speakeasy.generateSecret({length:20});
+            success.secret.push(secret.base32);
+            var token= speakeasy.totp({
+              secret: secret.base32,
+              encoding: 'base32'
+            });
+            twilioClient.messages.create({
+              from: from,
+              to: to,
+              body: `Your verification code is: $(token)`
+            });
+            res.render('verify', {data:found}); } })}
+      else {
+        res.send('Not eligible to vote. Person should be atleast 18 years of age.');
+        res.redirect('/register'); } }
+  });
+});
+
+app.post('/register/:adhar_id', function(req,res){
+  var userToken= req.body.token;
+  var secret= regVoter.find(req.params.adhar_id).secret;
+  var verified= speakeasy.totp.verify({
+    secret: secret,
+    encoding: 'base32',
+    token: userToken
+  });
+  if(verified) {
+  regVoter.find(req.params.adhar_id, function(err,success){
+  res.json(success);
+  })
+  } else {
+    res.send('Something went wrong');
+    res.redirect('/register');
+  }
+});
+
+app.get('/vote', function(req,res){
+  res.render('vote');
+});
+
+app.get('/vote/:adhar_id', function(req,res){
+  regVoter.find(req.params.adhar_id, function(err, found){
+    if(err) {
+      res.send('Data not found');
+      res.redirect('/vote'); }
+    else {
+            var token= speakeasy.totp({
+              secret: found.secret,
+              encoding: 'base32'
+            });
+            twilioClient.messages.create({
+              from: from,
+              to: to,
+              body: `Your verification code is: $(token)`
+            });
+            res.render('verify', {data:found}); 
+          } 
+        })
+});
+
+// app.post('/vote/:adhar_id', function(req,res){
+//   var userToken= req.body.token;
+//   var secret= regVoter.find(req.params.adhar_id).secret;
+//   var verified= speakeasy.totp.verify({
+//     secret: secret,
+//     encoding: 'base32',
+//     token: userToken
+//   });
+//   if(verified) {
+//     regVoter.find(req.params.adhar_id, function(err,success){
+//       res.json(success);
+//     })
+//   } else {
+//     res.send('Something went wrong');
+//     res.redirect('/vote');
+//   }
+// });
+
+app.listen(3000, function(){
+  console.log('Server Started');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -76,7 +76,6 @@ App={
                 {
                     App.error=0;
                     document.getElementById('votesParty').innerHTML='The total votes for party ',_party,' in constituency ',_constituency,' is ',instance.vote_count(_constituency,_party).toNumber();
-
                 }
                 else
                 {
@@ -227,6 +226,39 @@ App={
     },
 }
 
+function opennav() {
+    var nav=document.getElementById("hbutton");
+    if(nav.style.display == "none")
+        { nav.style.display = "block"; }
+    else {
+    nav.style.display = "none";
+    }
+}
+var ismobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+ 
+if (!ismobile){
+ window.addEventListener('scroll', function(){
+  var scrolled=document.getElementById('scrolled');
+  scrolled.style.top=-(window.pageYOffset * 0.5)+ 'px';
+ }, false)
+}
+var overlay = document.getElementById("overlay");
+var nav = document.getElementById("navv");
+window.addEventListener('load', function(){
+  overlay.style.display = 'none';
+  overlay.style.opacity= 0;
+  nav.style.opacity= 1;
+  nav.style.background= '#434342f7';
+})
+
+var ismobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+ 
+if (!ismobile){
+ window.addEventListener('scroll', function(){
+  var scrolled=document.getElementById('scrolled');
+  scrolled.style.top=-(window.pageYOffset * 0.5)+ 'px';
+ }, false)
+}
 window.onload = function(){
     App.init();
 }
